6a0d575014b81b3e906d1b49a3dbb4d8e01a8eba
ANIKET GUPTA <139427075+Aniket04087021@users.noreply.github.com>
2024-11-19 18:33:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript for toggling the mobile menu
+document.getElementById('menuToggle').addEventListener('click', function () {
+    const mobileMenu = document.getElementById('mobileMenu');
+    mobileMenu.classList.toggle('open');
+});
